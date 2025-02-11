8139930b40be61c16b039568976a3f09e364969b
Haji490 <99gkwldnjs99@gmail.com>
2025-02-11 13:52:19 +0900

    jsv5

---
+++ 05/05_1.js
@@ -0,0 +1,30 @@
+//alert("안녕하세요")
+
+//console에 출력
+console.log("안녕하세요.");
+
+
+////호이스팅
+////undefined
+// console.log(x); 
+// //결과값 1
+// var x = 1;
+// console.log(x);
+
+// //선언 안하고 사용하면 오류
+// //console.log(y);
+// let y = 1;
+// console.log(y);
+
+//변수 상수
+let x = 10;     //변수선언
+const y = 10;   //상수선언
+
+console.log("x = ", x, " y = ",y );         //나열
+console.log("x = "+ x + " y = "+ y );       //문자열 만듦
+console.log("x type = " , typeof(x));
+
+x = 'PNU';
+//y = 20;
+console.log("x = ", x, " y = ",y );
+console.log("x type = " , typeof(x));
